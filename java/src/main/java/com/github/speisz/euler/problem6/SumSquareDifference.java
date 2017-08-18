c11729af36f1181b10d35da879927904d0fcfed6
speisz <cspies486@gmail.com>
2017-08-18 15:28:42 +0200

    Problem 6, Java, iteration 1

---
+++ java/src/main/java/com/github/speisz/euler/problem6/SumSquareDifference.java
@@ -0,0 +1,28 @@
+package com.github.speisz.euler.problem6;
+
+import java.math.BigInteger;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+class SumSquareDifference {
+    static BigInteger computeFor(Supplier<Stream<BigInteger>> numbers) {
+        return sumSquare(numbers).subtract(squaresSum(numbers));
+    }
+
+    private static BigInteger squaresSum(Supplier<Stream<BigInteger>> numbers) {
+        return numbers.get()
+                .map(n -> n.pow(2))
+                .reduce(BigInteger::add)
+                .orElseThrow(IllegalStateException::new);
+    }
+
+    private static BigInteger sumSquare(Supplier<Stream<BigInteger>> numbers) {
+        return numbers.get()
+                .reduce(BigInteger::add)
+                .orElseThrow(IllegalStateException::new)
+                .pow(2);
+    }
+
+    private SumSquareDifference() {
+    }
+}
---
+++ java/src/test/java/com/github/speisz/euler/problem6/SumSquareDifferenceTest.java
@@ -0,0 +1,24 @@
+package com.github.speisz.euler.problem6;
+
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.stream.Stream;
+
+import static java.math.BigInteger.valueOf;
+import static java.util.stream.IntStream.rangeClosed;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class SumSquareDifferenceTest {
+    @Test
+    public void computesTheDifferenceOfSumSquareAndSquaresSum() {
+        assertThat(SumSquareDifference.computeFor(() -> numbersUpTo(10)), is(valueOf(2640)));
+        assertThat(SumSquareDifference.computeFor(() -> numbersUpTo(100)), is(valueOf(25164150)));
+        assertThat(SumSquareDifference.computeFor(() -> numbersUpTo(10000000)), is(new BigInteger("2500000166666641666665000000")));
+    }
+
+    private Stream<BigInteger> numbersUpTo(int upper) {
+        return rangeClosed(1, upper).boxed().map(BigInteger::valueOf);
+    }
+}
